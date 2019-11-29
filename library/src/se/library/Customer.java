618f3d6d39652661c57063555bf1b1193a554340
johaxn <johanna.k.axner@gmail.com>
2019-11-29 12:44:18 +0100

    Create class Customer

--- library/src/se/library/Customer.java
+++ library/src/se/library/Customer.java
@@ -5,10 +5,75 @@
  */
 package se.library;
 
-/**
- *
- * @author Stefan
- */
+import java.awt.print.Book;
+import java.util.ArrayList;
+
 public class Customer {
-    
+    private String name;
+    private String personalNo;
+    private String email;
+    private String password;
+    private String libraryCardNo;
+    private ArrayList<Book> borrowedBooks = new ArrayList<Book>();
+
+    //private ArrayList<Book> reservedBooks = new ArrayList<Book>();
+
+    public Customer(String name, String personalNo, String email, String password, String libraryCardNo) {
+        this.name = name;
+        this.personalNo = personalNo;
+        this.email = email;
+        this.password = password;
+        this.libraryCardNo = libraryCardNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPersonalNo() {
+        return personalNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLibraryCardNo() {
+        return libraryCardNo;
+    }
+
+    public ArrayList<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public ArrayList<Book> getReservedBooks() {
+        return reservedBooks;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", personalNo='" + personalNo + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", libraryCardNo='" + libraryCardNo + '\'' +
+                ", borrowedBooks=" + borrowedBooks +
+                '}';
+    }
+
+    public boolean isOver18(String personalNo) {
+        String dateOfBirth = personalNo.substring(0, 3);
+        int thisYear = 2019
+        int checkDateOfBirth = Integer.parseInt(dateOfBirth);
+        if (checkDateOfBirth - thisYear >= 18) {
+            return true;
+        } else
+            return false;
+    }
 }
+
