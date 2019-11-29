443aa0dfe9638dbe9c9eecbc393c4520813e71b9
sighBRG <55457027+sighBRG@users.noreply.github.com>
2019-11-29 13:00:18 +0100

    added books and created menu

--- library/src/se/library/bookList.java
+++ library/src/se/library/bookList.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package se.library;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author Stefan
  */
 public class bookList {
+    private ArrayList<String> books;
+        
+    public bookList() {
+        books = new ArrayList<>();    
+    }
+    
+    public String addBooks() {
+        books.add("Titel: " + "Sagan om ringen" + '\n' 
+                + "Författare: " + "J.R.R. Tolkien");
+        
+        
+        books.add("Titel: " + "Sagan om ringen" + '\n' 
+                + "Författare: " + "J.R.R. Tolkien");
+        books.add("Titel: " + "Sagan om ringen" + '\n' 
+                + "Författare: " + "J.R.R. Tolkien");
+        books.add("Titel: " + "Sagan om ringen" + '\n' 
+                + "Författare: " + "J.R.R. Tolkien");
+        books.add("Titel: " + "Sagan om ringen" + '\n' 
+                + "Författare: " + "J.R.R. Tolkien");
+        books.add("Titel: " + "Sagan om ringen" + '\n' 
+                + "Författare: " + "J.R.R. Tolkien");
+        books.add("Titel: " + "Sagan om ringen" + '\n' 
+                + "Författare: " + "J.R.R. Tolkien");
+        
+        
+        return null;
+    }
     
 }
