4e080f53dc73874d579335ffa4324d7fb5dd7cee
johaxn <johanna.k.axner@gmail.com>
2019-12-02 11:15:45 +0100

    Update Main.java

--- library/src/se/library/Main.java
+++ library/src/se/library/Main.java
@@ -12,15 +12,11 @@ package se.library;
 public class Main {
     public static void main(String[] args) {
         
-//        Menu menu = new Menu();
-//        menu.displayMenu();
-    	CustomerList cl=new CustomerList();
-    	cl.addOriginalCustomers();
+        Menu menu = new Menu();
+        menu.displayMenu();
     	
-    	for (Customer i:cl.customers){
-    		System.out.println(i);
     	}
     	
     }
     
-}
+
